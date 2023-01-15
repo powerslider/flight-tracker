89024694b807d7a91fabc6330a24ce5658b7c8ca
powerslider <tsvetan.dimitrov23@gmail.com>
2023-01-15 09:26:32 +0200

    Add cyclic dependency test case.

--- pkg/flights/tracker_test.go
+++ pkg/flights/tracker_test.go
@@ -3,6 +3,7 @@ package flights_test
 import (
 	"github.com/powerslider/flight-tracker/pkg/flights"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -70,5 +71,76 @@ func TestFlightTracker(t *testing.T) {
 		}
 
 		assert.Equal(t, actual, test.expected)
+	}
+}
+
+func TestFlightTrackerCyclicDependencies(t *testing.T) {
+	tests := []struct {
+		input    [][]flights.Airport
+		expected string
+	}{
+		{
+			input: [][]flights.Airport{
+				{"A", "B"},
+				{"B", "A"},
+			},
+			expected: "A -> B -> A",
+		},
+		{
+			input: [][]flights.Airport{
+				{"A", "B"},
+				{"B", "C"},
+				{"C", "B"},
+			},
+			expected: "B -> C -> B",
+		},
+		{
+			input: [][]flights.Airport{
+				{"A", "B"},
+				{"B", "C"},
+				{"C", "A"},
+			},
+			expected: "A -> B -> C -> A",
+		},
+		{
+			input: [][]flights.Airport{
+				{"SFO", "EWR"},
+				{"EWR", "SFO"},
+			},
+			expected: "SFO -> EWR -> SFO",
+		},
+		{
+			input: [][]flights.Airport{
+				{"SFO", "EWR"},
+				{"EWR", "ATL"},
+				{"ATL", "EWR"},
+			},
+			expected: "EWR -> ATL -> EWR",
+		},
+		{
+			input: [][]flights.Airport{
+				{"SFO", "EWR"},
+				{"EWR", "ATL"},
+				{"ATL", "SFO"},
+			},
+			expected: "SFO -> EWR -> ATL -> SFO",
+		},
+	}
+
+	for _, test := range tests {
+		tracker := flights.NewTracker()
+
+		for _, row := range test.input {
+			tracker.AddFlightRoute(row[0], row[1])
+		}
+
+		_, err := tracker.Trace()
+		if err == nil {
+			t.Error("Expected cycle error did not occur!")
+		}
+
+		if !strings.Contains(err.Error(), test.expected) {
+			t.Errorf("Error does not print cycle: %q", err)
+		}
 	}
 }
